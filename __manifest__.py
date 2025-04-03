f28e7bac34da260e13ce227b16797b24dd1bc367
Aiduoxila <159774819+Aiduoxila@users.noreply.github.com>
2025-04-03 14:36:26 +0200

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Allegro.CZ Integration',
+    'version': '1.0',
+    'summary': 'Integrates Allegro.CZ API with Odoo',
+    'author': 'Your Name or Company',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': ['data/api_config.xml'],
+    'application': True,
+}
